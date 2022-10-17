fdf01623f0825ea78bcb06803077e13400f7d046
Rdoria96 <rddm096@gmail.com>
2022-10-17 11:51:31 -0500

    actualizacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
-
+import { HomeComponent } from "./home/home.component"
+import { AboutComponent } from "./about/about.component"
+import { ProductsComponent } from "./products/products.component"
+import { SingleProductComponent } from "./single-product/single-product.component"
+import { ContactUsComponent } from "./contact-us/contact-us.component"
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "about", component: AboutComponent },
+  { path: "products", component: ProductsComponent },
+  { path: "single-product", component: SingleProductComponent },
+  { path: "contact-us", component: ContactUsComponent }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,23 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { BodyComponent } from './body/body.component';
+import { AboutComponent } from './about/about.component';
+import { ProductsComponent } from './products/products.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { SingleProductComponent } from './single-product/single-product.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    BodyComponent
+    BodyComponent,
+    AboutComponent,
+    ProductsComponent,
+    ContactUsComponent,
+    SingleProductComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
